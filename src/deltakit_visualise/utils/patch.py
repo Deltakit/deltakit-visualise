cec9e281fac5b87f2d79d639bc415b3841e2e4fa
Jatin <140497406+jatinriverlane@users.noreply.github.com>
2026-09-02 12:14:39 +0100

    feat(single-patch): add feature for single patch visualisation (#11)
    * Add feature for single patch visualisation
    
    * replace dkit_compile with deltakit_compile adn dkit_visualise with deltakit_visualise
    
    * fix failing ruff error
    
    * remove unneeded copyright content and clean after rebase

---
+++ src/deltakit_visualise/utils/patch.py
@@ -0,0 +1,26 @@
+# (c) Copyright Riverlane 2025-2026. All rights reserved.
+"""Utility function to visualise a single logical patch from a LogAsmBuilder."""
+
+from pathlib import Path
+
+from deltakit_compile.frontend.logasm import LogAsmBuilder, RotatedPlanarPatch
+
+from deltakit_visualise.logical_assembly_api_visualiser import LogAsmAPIVisualiser
+
+
+def visualise_logical_patch(
+    builder: LogAsmBuilder, patch: RotatedPlanarPatch, round_no: int = 1
+) -> Path:
+    """Visualise a logical patch.
+
+    Args:
+        builder: The logical assembly builder.
+        patch: The logical patch to visualise.
+        round_no: Optional round number to visualise. Defaults to 1. If provided,
+            only operations for that specified round are included.
+
+    Returns:
+        A Path to the generated visualisation.
+    """
+    api_visualiser = LogAsmAPIVisualiser(builder)
+    return api_visualiser.visualise_logical_patch(patch, round_no)
